cdb3a174979cf60f3ca1f507a3c0180a84846d7b
raonever <ever29lkh@gmail.com>
2020-09-24 13:17:28 +0900

    sub 21~22 array, 23~24 fs.readdir

--- main.js
+++ main.js
@@ -9,55 +9,65 @@ var app = http.createServer(function(request,response){
 
     if(pathname === '/'){
       if(queryData.id === undefined) {
-        // fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {   // 읽어들일 파일이 없으므로 생략 가능, queryData.id는 undefined
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="?id=HTML">HTML</a></li>
-              <li><a href="?id=CSS">CSS</a></li>
-              <li><a href="?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
-        // });
+
+        fs.readdir('./data', function(error, filelist){
+          console.log(filelist);
+            var title = 'Welcome';
+            var description = 'Hello, Node.js';
+            var list = '<ul>';
+            var i = 0;
+            while(i < filelist.length) {
+              list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+              i = i + 1;
+            }
+            list = list + '<ul>';
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+        });
       } else {
-        fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
-          var title = queryData.id;
-          var template = `
-          <!doctype html>
-          <html>
-          <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-          </head>
-          <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-              <li><a href="?id=HTML">HTML</a></li>
-              <li><a href="?id=CSS">CSS</a></li>
-              <li><a href="?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>${description}</p>
-          </body>
-          </html>
-          `;
-          response.writeHead(200);
-          response.end(template);
+        fs.readdir('./data', function(error, filelist){
+          var list = '<ul>';
+          var i = 0;
+          while(i < filelist.length) {
+            list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
+            i = i + 1;
+          }
+          list = list + '<ul>';
+          fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description) {
+            var title = queryData.id;
+            var template = `
+            <!doctype html>
+            <html>
+            <head>
+              <title>WEB1 - ${title}</title>
+              <meta charset="utf-8">
+            </head>
+            <body>
+              <h1><a href="/">WEB</a></h1>
+              ${list}
+              <h2>${title}</h2>
+              <p>${description}</p>
+            </body>
+            </html>
+            `;
+            response.writeHead(200);
+            response.end(template);
+          });
         });
       }
     } else {
---
+++ syntax/array.js
@@ -0,0 +1,8 @@
+var arr =['A','B','C','D'];
+console.log(arr[1]);
+console.log(arr[3]);
+arr[2] = 3;
+console.log(arr);    // 배열은 0부터 셈
+console.log(arr.length);  // length는 1부터 셈
+arr.push('E');
+console.log(arr);
